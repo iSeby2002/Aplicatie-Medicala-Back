7e988e78d0d1fb6ad666a3121e95b963a907a1e9
iSeby2002 <115586126+iSeby2002@users.noreply.github.com>
2024-08-02 14:18:10 +0300

    mm

--- src/main/java/com/example/demo/AplicatieProgramariMedicaleApplication.java
+++ src/main/java/com/example/demo/AplicatieProgramariMedicaleApplication.java
@@ -11,22 +11,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.sql.SQLOutput;
 import java.time.LocalDate;
 
-@Controller
 @SpringBootApplication
 public class AplicatieProgramariMedicaleApplication {
 
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World!";
-	}
+//	@RequestMapping("/")
+//	@ResponseBody
+//	String home() {
+//		return "Hello World!";
+//	}
 
 	public static void main(String[] args) {
 
